c93d98c358695e2b55f5104b37ae2e2f42081d6a
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-07-12 01:27:12 +0200

    feat: connect NestJS to Postgres via TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.getOrThrow<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT', 5432),
+        username: config.getOrThrow<string>('DB_USERNAME'),
+        password: config.getOrThrow<string>('DB_PASSWORD'),
+        database: config.getOrThrow<string>('DB_NAME'),
+        entities: [],
+        synchronize: false,
+        logging: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
